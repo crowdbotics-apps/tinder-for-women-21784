55a779e7cc8856bf2e41a4d50ccde0a35a97bead
Crowdbotics <team@crowdbotics.com>
2020-10-21 11:32:36 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/tinder_for_women_21784/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tinder_for_women_21784;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "tinder_for_women_21784";
+    }
+}
